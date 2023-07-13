ad348991881543b58c761b24b9f3f78ddb05d7f4
Joseph Concillado <josephjconcillado@gmail.com>
2023-07-13 15:19:20 -0400

    Added icons in asset folder, created UIViewControllers and linked to respective classes OnboardViewController, ProductsViewController, ReportViewController, BackupViewController, SettingsViewController, AccountSettingsViewController, AboutSettingsViewController, BarcodeScannerViewController, ProductDetailsViewController, AddProductViewController.

---
+++ inventoTrack/Report/ReportViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ReportViewController.swift
+//  inventoTrack
+//
+//  Created by jconcillado on 2023-07-13.
+//
+
+import UIKit
+
+class ReportViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
