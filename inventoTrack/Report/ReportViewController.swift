c8c14f3554216c4b22ffb5df319d21adcf06d701
Joseph Concillado <josephjconcillado@gmail.com>
2023-07-15 01:09:23 -0400

    Updated ReportViewController code and added UIView linked to piechartView with save Report functionality as csv file

--- inventoTrack/Report/ReportViewController.swift
+++ inventoTrack/Report/ReportViewController.swift
@@ -6,24 +6,109 @@
 //
 
 import UIKit
+import DGCharts
 
 class ReportViewController: UIViewController {
-
+    
+    @IBOutlet weak var chartView: PieChartView!
+    //    @IBOutlet weak var pieChart: PieChartView!
+    
+    private var allProducts: [Product] = [] {
+        didSet {
+            filteredProducts = allProducts
+        }
+    }
+    private var filteredProducts: [Product] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        override func viewDidLayoutSubviews() {
+            super.viewDidLayoutSubviews()
+    
+            allProducts = CoreDataManager.shared.fetchProducts(sortOption: "pDateCreated")
+    
+            var entries = [ChartDataEntry]()
+            for value in filteredProducts {
+                entries.append(PieChartDataEntry(value: Double(value.pQty), label: value.pName))
+            }
+    
+            let numberOfItems = entries.count
+                    let legendHeight = CGFloat(30 * numberOfItems) // Assuming each legend item has a height of 30 pixels
+    
+                    // Adjust the chart view frame to fit the legend
+            chartView.frame.size.height += legendHeight
+    //        chartView.frame = CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: self.view.frame.size.width)
+            chartView.center = view.center
+            chartView.centerText = "InventoTrack"
+            chartView.drawEntryLabelsEnabled = false
+            chartView.legendRenderer.legend?.orientation = .vertical
+    //        chartView.legendRenderer.legend?.horizontalAlignment = .left
+    
+            chartView.animate(xAxisDuration: 1)
+            view.addSubview(chartView)
+    
+    
+    
+            let set  = PieChartDataSet(entries: entries, label: "")
+            set.colors = ChartColorTemplates.colorful()
+            let data = PieChartData(dataSet: set)
+            chartView.data = data
+    
+        }
+    
+    @IBAction func saveReport(_ sender: Any) {
+        print("Start exporting...")
+        let fileManager = FileManager.default
+        guard let documentsDirectory = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first else {
+            return
+        }
+        
+        let file_name = "My Inventory Report.csv"
+        //        let path = NSURL(fileURLWithPath: NSTemporaryDirectory()).appendingPathComponent(file_name)
+        let path = documentsDirectory.appendingPathComponent(file_name)
+        var csvHeader = "Barcode,Product,Description,Quantity,Price\n"
+        
+        for product in filteredProducts {
+            csvHeader.append("\(product.pBarcode!),\(product.pName!),\(product.pDescription!),\(product.pQty),\(product.pPrice)\n")
+        }
+        
+        do {
+            try csvHeader.write(to: path, atomically: true, encoding: .utf8)
+            let exportSheet = UIActivityViewController(activityItems: [path as Any], applicationActivities: nil)
+            exportSheet.completionWithItemsHandler = { [weak self] (activityType, completed, returnedItems, error) in
+                
+                if completed {
+                    self?.showAlert(withTitle: "inventoTrack", message: "Report Successfully Saved!")
+                }
+                
+                // Dismiss the view controller when sharing/exporting is completed or canceled
+                self?.deleteTempFile(url: path)
+            }
+            self.present(exportSheet, animated: true, completion: nil)
+            
+        }catch {
+            print("Error")
+        }
     }
-    */
-
+    
+    func deleteTempFile(url: URL) {
+        do {
+            try FileManager.default.removeItem(at: url)
+            print("Removed Temp JSON File")
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+    private func showAlert(withTitle title: String, message: String) {
+        DispatchQueue.main.async {
+            let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+            alertController.addAction(UIAlertAction(title: "OK", style: .default))
+            self.present(alertController, animated: true)
+        }
+    }
+    
 }
